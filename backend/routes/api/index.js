c479743509cc53505b6973be65ef30c97e40afb2
Zach <hoschar.zach@gmail.com>
2022-06-29 17:45:58 -0500

    add user login endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,16 +1,29 @@
 const router = require('express').Router()
-const { setTokenCookie } = require('../../utils/auth.js')
-const { User } = require('../../db/models')
+const sessionRouter = require('./session.js')
+const usersRouter = require('./users.js')
 const { restoreUser } = require('../../utils/auth.js')
-const { requireAuth } = require('../../utils/auth.js')
+
+// IMPORTS FOR TESTING
+
+// const { setTokenCookie } = require('../../utils/auth.js')
+// const { User } = require('../../db/models')
+// const { requireAuth } = require('../../utils/auth.js')
+
 
 // if current user session is valid, req.user is the user in the database
 // if the current sessions is not valid, set req.user to null
 router.use(restoreUser)
 
+router.use('/session', sessionRouter)
+
+router.use('users', usersRouter)
+
+router.post('/test/', (req, res) => {
+    res.json({requestBody: req.body})
+})
 
 
-
+module.exports = router
 
 
 
@@ -40,4 +53,3 @@ router.use(restoreUser)
 // router.post('/test', (req, res) => {
 //     res.json({requestBody: req.body})
 // })
-module.exports = router
