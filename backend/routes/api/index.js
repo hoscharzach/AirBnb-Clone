efa7966acb956edc815e4568bb67771675285cb6
Zach <hoschar.zach@gmail.com>
2022-06-29 16:46:28 -0500

    finished phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,28 +4,40 @@ const { User } = require('../../db/models')
 const { restoreUser } = require('../../utils/auth.js')
 const { requireAuth } = require('../../utils/auth.js')
 
-
+// if current user session is valid, req.user is the user in the database
+// if the current sessions is not valid, set req.user to null
 router.use(restoreUser)
 
-router.get('/restore-user', async (req, res) => {
-    return res.json(req.user)
-})
 
-router.get('/require-auth', requireAuth, async (req, res) => {
-    return res.json(req.user)
-})
 
-router.get('/set-token-cookie', async (_req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        }
-    })
-    setTokenCookie(res, user)
-    return res.json({ user })
-})
-// send back the request body when interacting with the '/api/test' endpoint
-router.post('/test', (req, res) => {
-    res.json({requestBody: req.body})
-})
+
+
+
+
+
+
+
+// TESTING PURPOSES
+
+// router.get('/restore-user', async (req, res) => {
+//     return res.json(req.user)
+// })
+
+// router.get('/require-auth', requireAuth, async (req, res) => {
+//     return res.json(req.user)
+// })
+
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     })
+//     setTokenCookie(res, user)
+//     return res.json({ user })
+// })
+// // send back the request body when interacting with the '/api/test' endpoint
+// router.post('/test', (req, res) => {
+//     res.json({requestBody: req.body})
+// })
 module.exports = router
