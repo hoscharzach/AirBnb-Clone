09c0534b498ad8765e4cf9c641b8916cb18434d1
Zach <hoschar.zach@gmail.com>
2022-07-05 12:18:53 -0500

    finished get current users spots

--- backend/routes/api/currentUser.js
+++ backend/routes/api/currentUser.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const { User, Spot } = require('../../db/models')
 const { requireAuth, restoreUser } = require('../../utils/auth')
+const { check } = require('express-validator')
+const { handleValidationErrors } = require('../../utils/validation')
 const router = express.Router()
+
+// const validateSpot = [
+//     check('ownerId')
+//       .exists({ checkFalsy: true })
+//       .withMessage('Please provide a valid email.'),
+//     handleValidationErrors
+//   ];
 
 router.get('/spots', [restoreUser, requireAuth], async (req, res, next) => {
     if (req.user) {
@@ -21,6 +30,24 @@ router.get('/spots', [restoreUser, requireAuth], async (req, res, next) => {
         const err = new Error ("User not found")
         res.status = 404
         return res.json(err)
+    }
+})
+
+router.post('/spots', [restoreUser, requireAuth], async (req, res, next) => {
+
+    const { address, city, state, country, lat, lng, name, description, price } = req.body
+
+    requiredFields.forEach(el => {
+
+      if (req.body[el] === undefined) {
+        return res.json({
+          yo: req.body
+        })
+      }
+    })
+
+    if (req.user) {
+      res.json({yo: "hello"})
     }
 })
 
