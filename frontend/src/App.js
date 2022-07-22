c534905d3e8edd1c4e043ecdf291eebce691af42
Zach <hoschar.zach@gmail.com>
2022-07-22 12:16:45 -0500

    imports messed up?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
-import { thunkLogin, thunkLogout } from './store/session'
-import { useDispatch } from 'react-redux'
 import LoginFormPage from './components/LoginFormPage/LoginFormPage';
-import { Switch, Route } from 'react-router-dom'
+import { logout } from ''./store/session''
+import { useDispatch } from 'react-redux'
+import { Switch, Route, Link } from 'react-router-dom'
 
 function App() {
+  const dispatch = useDispatch()
+
+
   return (
     <>
     <Switch>
       <Route exact path="/">
+        <>
         <h1>Hello from home</h1>
+        <Link to="/login"><button>Login</button></Link><br></br>
+        <button onClick={(e) => dispatch(logout)}>Logout</button>
+        </>
       </Route>
       <Route path="/login">
     <LoginFormPage />
