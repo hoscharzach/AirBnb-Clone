80581fad2f08507c9cd1c926e720d1b4fdc02ce9
Zach <hoschar.zach@gmail.com>
2022-07-22 17:48:45 -0500

    sign up working correctly

--- frontend/src/components/SignupForm/index.js
+++ frontend/src/components/SignupForm/index.js
@@ -1,10 +1,12 @@
 import './signupform.css'
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { thunkSignup } from '../../store/session'
+import { Redirect } from 'react-router-dom'
 
 export default function SignupForm() {
     const dispatch = useDispatch()
+    const user = useSelector(state => state.session.user)
 
     const [username, setUsername] = useState('')
     const [firstName, setFirstName] = useState('')
@@ -14,24 +16,28 @@ export default function SignupForm() {
     const [errors, setErrors] = useState([])
     const [confirmPassword, setConfirmPassword] = useState('')
 
+    if (user) return <Redirect to="/" />
+
     function onSubmit (e) {
         e.preventDefault()
         setErrors([])
 
-        const payload = {
-            username,
-            email,
-            firstName,
-            lastName,
-            password,
-        }
+        if (password === confirmPassword) {
+            const payload = {
+                username,
+                email,
+                firstName,
+                lastName,
+                password,
+            }
 
-        dispatch(thunkSignup(payload))
-        .catch(async (res) => {
-            const data = await res.json();
-            console.log(data)
-            if (data && data.errors) setErrors(data.errors);
-          });
+            dispatch(thunkSignup(payload))
+            .catch(async (res) => {
+                const data = await res.json();
+                console.log(data)
+                if (data && data.errors) setErrors(data.errors);
+              });
+        } else setErrors(["Passwords do not match"])
     }
 
     return (
@@ -44,11 +50,11 @@ export default function SignupForm() {
                 ))}
             </ul>
             <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username"></input>
-            <input value={firstName} onChange={(e) => setFirstName(e.target.value)} type="text" placeholder="First Name"></input>
-            <input value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" placeholder="LastName"></input>
+            <input value={firstName} onChange={(e) => setFirstName(e.target.value)} type="text" placeholder="First name"></input>
+            <input value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" placeholder="Last name"></input>
             <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email"></input>
             <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password"></input>
-            <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type="password" placeholder="Password"></input>
+            <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type="password" placeholder="Confirm password"></input>
             <button>Sign Up</button>
         </form>
         </>
