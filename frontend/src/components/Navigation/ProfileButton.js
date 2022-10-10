02a120d2aa21e4722d6c0cd932bccd5d2fee2d27
Zach <hoschar.zach@gmail.com>
2022-10-10 15:55:40 -0500

    test

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -8,77 +8,77 @@ import LoginFormModal from '../LoginFormModal'
 
 
 function ProfileButton({ user }) {
-    const sessionUser = useSelector(state => state.session.user)
-    const dispatch = useDispatch();
-    const [showMenu, setShowMenu] = useState(false);
-    const history = useHistory()
+  const sessionUser = useSelector(state => state.session.user)
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const history = useHistory()
 
-    const openMenu = () => {
-      if (showMenu) return;
-      setShowMenu(true);
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      const modal = document.getElementById('modal-background')
+      if (modal) return
+
+      if (e.target.id === 'login-form-modal' ||
+        e.target.id === 'signup-form-modal' ||
+        e.target.id === 'modal-background') return
+
+      setShowMenu(false);
     };
 
-    useEffect(() => {
-      if (!showMenu) return;
+    document.addEventListener('click', closeMenu);
 
-      const closeMenu = (e) => {
-        const modal = document.getElementById('modal-background')
-        if( modal) return
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
-        if (e.target.id === 'login-form-modal'||
-            e.target.id === 'signup-form-modal' ||
-            e.target.id === 'modal-background') return
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.thunkLogout());
+    history.push('/')
+  };
 
-        setShowMenu(false);
-      };
-
-      document.addEventListener('click', closeMenu);
-
-      return () => document.removeEventListener("click", closeMenu);
-    }, [showMenu]);
-
-    const logout = (e) => {
-      e.preventDefault();
-      dispatch(sessionActions.thunkLogout());
-      history.push('/')
-    };
-
-    let dropdownItems
-    if (user) {
-      dropdownItems = (
-        <>
-          <div className='menu-item'>{user.username}</div>
-          <div className='menu-item menu-item-hover' onClick={() => history.push('/my-profile')} >My Profile</div>
-          <div className='menu-item menu-item-hover' onClick={() => history.push('/create-listing')} >Create Listing</div>
-          <div className='menu-item menu-item-hover' onClick={logout}>Log out</div>
-        </>
-      )
-    }
-
-    if (!user) {
-      dropdownItems = (
-        <>
-          <LoginFormModal />
-          <div className='menu-item menu-item-hover' onClick={() => history.push('/signup')} >Sign up</div>
-        </>
-      )
-    }
-
-    return (
-      <div id="menu-container">
-        <div className="profile-icons-container" onClick={openMenu}>
-          <button className="profile-icons">
-            <img className='hamburger-icon' src={hamburgerIcon} alt="" />
-            <img className='user-icon' src={userIcon} alt="" />
-          </button>
-        </div>
-        {showMenu && (
-          <div id="profile-dropdown-wrapper">
-            {dropdownItems}
-          </div>
-        )}
-      </div>
-    );
+  let dropdownItems
+  if (user) {
+    dropdownItems = (
+      <>
+        <div className='menu-item'>{user.username}</div>
+        <div className='menu-item menu-item-hover' onClick={() => history.push('/my-profile')} >My Profile</div>
+        <div className='menu-item menu-item-hover' onClick={() => history.push('/create-listing')} >Create Listing</div>
+        <div className='menu-item menu-item-hover' onClick={logout}>Log out</div>
+      </>
+    )
   }
 
-  export default ProfileButton;
+  if (!user) {
+    dropdownItems = (
+      <>
+        <LoginFormModal />
+        <div className='menu-item menu-item-hover' onClick={() => history.push('/signup')} >Sign up</div>
+      </>
+    )
+  }
+
+  return (
+    <div className='flex'>
+      <div className="flex" onClick={openMenu}>
+        <button className='flex items-center'>
+          <img className='hamburger-icon' src={hamburgerIcon} alt="" />
+          <img className='user-icon' src={userIcon} alt="" />
+        </button>
+      </div>
+      {showMenu && (
+        <div id="profile-dropdown-wrapper">
+          {dropdownItems}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ProfileButton;
