6434116c5031f765c671b854f484ae191f84ded9
Zach <hoschar.zach@gmail.com>
2022-10-10 14:34:05 -0500

    added files to tracking??

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -6,56 +6,56 @@ import * as sessionActions from '../../store/session'
 import mainLogo from '../../assets/images/icons/svgexport-1.svg'
 
 
-function Navigation({ isLoaded }){
-    const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector(state => state.session.user);
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const demoLogin = () => {
-      dispatch(sessionActions.thunkLogin({
-        credential: 'Demo-lition',
-        password: 'password'
-      }))
+  const demoLogin = () => {
+    dispatch(sessionActions.thunkLogin({
+      credential: 'Demo-lition',
+      password: 'password'
+    }))
 
-    }
+  }
 
-    let sessionLinks;
-    if (sessionUser) {
-      sessionLinks = (
-        <>
-          <Link to="/create-listing">Become a Host</Link>
-          <ProfileButton user={sessionUser} />
-        </>
-      );
-    }
-    else {
-      sessionLinks = (
-        <>
-          <button id="demo-user-button" onClick={demoLogin}>Demouser</button>
-          <ProfileButton user={sessionUser} />
-        </>
-      );
-    }
-
-    return (
-      <div className="entire-header">
-
-        <div className="header-container">
-
-          <div className="logo-container">
-            <NavLink exact to="/">
-              <img className="header-icon" src={mainLogo} alt=" "></img>
-            </NavLink>
-          </div>
-
-          <div className="home-nav-links">
-            {isLoaded && sessionLinks}
-          </div>
-
-        </div>
-      </div>
-
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <>
+        <Link to="/create-listing">Become a Host</Link>
+        <ProfileButton user={sessionUser} />
+      </>
+    );
+  }
+  else {
+    sessionLinks = (
+      <>
+        <button id="demo-user-button" onClick={demoLogin}>Demouser</button>
+        <ProfileButton user={sessionUser} />
+      </>
     );
   }
 
-  export default Navigation;
+  return (
+    <div className="w-full h-24 flex ">
+
+      <div className="w- ">
+
+        <div className="logo-container">
+          <NavLink exact to="/">
+            <img className="header-icon" src={mainLogo} alt=" "></img>
+          </NavLink>
+        </div>
+
+        <div className="home-nav-links">
+          {isLoaded && sessionLinks}
+        </div>
+
+      </div>
+    </div>
+
+  );
+}
+
+export default Navigation;
